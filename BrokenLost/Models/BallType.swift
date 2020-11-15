8a5540693f54a772c7d1af5dfff6f9be1d45f85d
Исламгулов Артур <islamgulov@technokratos.com>
2020-11-15 15:38:43 +0300

    Added BallType. Added tap/swipe for balls

---
+++ BrokenLost/Models/BallType.swift
@@ -0,0 +1,13 @@
+//
+//  BallType.swift
+//  BrokenLost
+//
+//  Created by Artur Islamgulov on 15.11.2020.
+//
+
+import Foundation
+
+enum BallType {
+    case broken
+    case lost
+}
--- BrokenLost/Screens/Game/GameViewController.swift
+++ BrokenLost/Screens/Game/GameViewController.swift
@@ -11,7 +11,7 @@ class GameViewController: UIViewController {
     
     private enum Constants {
         static let ballsCount: Int = 10
-        static let ballRadius: CGFloat = 25
+        static let ballDiameter: CGFloat = 60
     }
     
     override func viewDidLoad() {
@@ -28,28 +28,70 @@ class GameViewController: UIViewController {
     private func setupBalls() {
         
         let bounds = view.frame
-        let maxX = bounds.maxX
-        let maxY = bounds.maxY
+        let maxX = bounds.maxX - Constants.ballDiameter
+        let maxY = bounds.maxY - Constants.ballDiameter
         
         for i in 0...Constants.ballsCount {
             let ball = UIView()
             view.addSubview(ball)
-            ball.frame.size = CGSize(width: Constants.ballRadius * 2,
-                                     height: Constants.ballRadius * 2)
+            ball.frame.size = CGSize(width: Constants.ballDiameter,
+                                     height: Constants.ballDiameter)
             
             let ballX = CGFloat.random(in: 0..<maxX)
             let ballY = CGFloat.random(in: 0..<maxY)
             ball.frame.origin = CGPoint(x: ballX, y: ballY)
             
-            ball.layer.cornerRadius = Constants.ballRadius
+            ball.layer.cornerRadius = Constants.ballDiameter / 2
             ball.layer.masksToBounds = true
+            
+            setupShadow(for: ball)
             
             if i % 2 == 0 {
                 ball.backgroundColor = .red
+                handleAction(ball, type: .broken)
             } else {
                 ball.backgroundColor = .blue
+                handleAction(ball, type: .lost)
             }
         }
+    }
+    
+    private func handleAction(_ view: UIView, type: BallType) {
+        view.isUserInteractionEnabled = true
+        switch type {
+        case .broken:
+            let tap = UITapGestureRecognizer(target: self, action: #selector(brokeBall(_:)))
+            view.addGestureRecognizer(tap)
+        case .lost:
+            let swipe = UISwipeGestureRecognizer(target: self, action: #selector(lostBall(_:)))
+            view.addGestureRecognizer(swipe)
+        }
+    }
+    
+    @objc
+    private func brokeBall(_ gesture: UITapGestureRecognizer) {
+        let view = gesture.view
+        view?.removeFromSuperview()
+    }
+    
+    @objc
+    private func lostBall(_ gesture: UISwipeGestureRecognizer) {
+        let view = gesture.view
+        view?.removeFromSuperview()
+    }
+    
+    @objc
+    private func error(_ gesture: UISwipeGestureRecognizer) {
+        let view = gesture.view
+        view?.removeFromSuperview()
+    }
+    
+    private func setupShadow(for view: UIView) {
+        view.layer.shadowColor = UIColor.black.cgColor
+        view.layer.shadowOffset = .init(width: 1, height: 1)
+        view.layer.shadowOpacity = 0.5
+        view.layer.shadowRadius = 5
+        view.layer.masksToBounds = false
     }
 
 }
